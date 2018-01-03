0ee58c2392a3a798fa0fa05f5770b544a3cadd3e
robotaleh <alex.santos.ete@gmail.com>
2018-01-03 07:13:27 +0100

    Añade la segunda Activity PIDManager y su layout inicial en landscape

---
+++ app/src/main/java/com/oprobots/robotaleh/pidfrombt/PIDManager.java
@@ -0,0 +1,13 @@
+package com.oprobots.robotaleh.pidfrombt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PIDManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pidmanager);
+    }
+}
