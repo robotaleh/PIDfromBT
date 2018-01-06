c97b4346e4c3847f865935f5e728f3e0b47f17f4
robotaleh <alex.santos.ete@gmail.com>
2018-01-06 13:11:08 +0100

    Mejora el layout de la activity PIDmanager, ajustando el tamaño de la consola y los márgenes.

--- app/src/main/java/com/oprobots/robotaleh/pidfrombt/PIDManager.java
+++ app/src/main/java/com/oprobots/robotaleh/pidfrombt/PIDManager.java
@@ -2,6 +2,9 @@ package com.oprobots.robotaleh.pidfrombt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 public class PIDManager extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class PIDManager extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pidmanager);
+
+        TextView consola = (TextView) findViewById(R.id.consola);
+        consola.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void onChangeButton(View view) {
     }
 }
